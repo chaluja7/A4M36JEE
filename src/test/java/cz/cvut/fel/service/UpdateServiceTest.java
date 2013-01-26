0ce33e8712dad8258f7fad83d37d9f5f2ace73a6
Karel Cemus <karel.cemus@gmail.com>
2013-01-26 21:44:09 +0100

    WebService security
     - defined role 'webservice' with authorization for WebMethod execution
     - updated JAX-WS configuration
     - updated test, added test for new user in proper role

--- src/test/java/cz/cvut/fel/service/UpdateServiceTest.java
+++ src/test/java/cz/cvut/fel/service/UpdateServiceTest.java
@@ -3,8 +3,10 @@ package cz.cvut.fel.service;
 import cz.cvut.fel.model.FlightStatus;
 import cz.cvut.fel.util.ArquillianTest;
 import cz.cvut.fel.util.UpdateServiceClient;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import static cz.cvut.fel.utils.DateUtils.date;
@@ -15,21 +17,32 @@ public class UpdateServiceTest extends ArquillianTest {
     /** The path of the WSDL endpoint in relation to the deployed web application. */
     private static final String WSDL_PATH = "http://localhost:8080/FlightSystem/UpdateService?wsdl";
 
+    /** used WS client to test the service */
     private UpdateServiceClient client;
 
-    @Test
-    public void testUpdate() throws Exception {
-        client = new UpdateServiceClient( new URL( WSDL_PATH ) );
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        if ( isInContainer() ) {
+            client = new UpdateServiceClient( new URL( WSDL_PATH ) );
+        }
+    }
+
+    @Test( expectedExceptions = Exception.class )
+    public void testUpdate_NotEvenAdminCan() {
         client.login( "karel", "cemus" );
         client.update( "1", date( 1, 2, 2013, 10, 20 ), date( 1, 2, 2013, 13, 20 ), FlightStatus.DELAYED );
     }
 
     @Test( expectedExceptions = Exception.class )
-    public void testUpdate_NotInRole() throws Exception {
-        client = new UpdateServiceClient( new URL( WSDL_PATH ) );
+    public void testUpdate_NotInRole() {
         client.login( "lubos", "matl" );
         client.update( "1", date( 1, 2, 2013, 10, 20 ), date( 1, 2, 2013, 13, 20 ), FlightStatus.DELAYED );
     }
 
+    @Test
+    public void testUpdate() {
+        client.login( "petr", "praus" );
+        client.update( "1", date( 1, 2, 2013, 10, 20 ), date( 1, 2, 2013, 13, 20 ), FlightStatus.DELAYED );
+    }
 
 }
